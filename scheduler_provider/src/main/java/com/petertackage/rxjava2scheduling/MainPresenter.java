8104f683821fd0ff11dc86b7107aa76b6a0fa0be
Zhilu Tang <tonythompsoncmu@gmail.com>
2025-01-11 20:29:06 -0800

    Add more apps.

---
+++ scheduler_provider/src/main/java/com/petertackage/rxjava2scheduling/MainPresenter.java
@@ -0,0 +1,40 @@
+package com.petertackage.rxjava2scheduling;
+
+
+import androidx.annotation.VisibleForTesting;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.disposables.CompositeDisposable;
+
+final class MainPresenter {
+
+    @VisibleForTesting
+    static final long DELAY_TIME_SEC = 5;
+
+    private final BookstoreModel bookstoreModel;
+    private final BookView view;
+    private final SchedulerProvider schedulerProvider;
+    private final CompositeDisposable disposable = new CompositeDisposable();
+
+    MainPresenter(BookstoreModel bookstoreModel,
+                  BookView view,
+                  SchedulerProvider schedulerProvider) {
+        this.bookstoreModel = bookstoreModel;
+        this.view = view;
+        this.schedulerProvider = schedulerProvider;
+    }
+
+    void bind() {
+        disposable.add(bookstoreModel.getFavoriteBook()
+                                     .map(Book::getTitle)
+                                     .delay(DELAY_TIME_SEC, TimeUnit.SECONDS,
+                                            schedulerProvider.computation())
+                                     .observeOn(schedulerProvider.ui())
+                                     .subscribe(view::setBookTitle));
+    }
+
+    void unbind() {
+        disposable.clear();
+    }
+
+}
---
+++ scheduler_provider/src/test/java/com/petertackage/rxjava2scheduling/MainPresenterTest.java
@@ -0,0 +1,62 @@
+package com.petertackage.rxjava2scheduling;
+
+import com.petertackage.rxjava2scheduling.test.TestSchedulerProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.TestScheduler;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MainPresenterTest {
+
+    @Mock
+    private BookstoreModel bookstoreModel;
+
+    @Mock
+    private BookView bookView;
+
+    private TestScheduler testScheduler;
+
+    private MainPresenter presenter;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        testScheduler = new TestScheduler();
+        presenter = new MainPresenter(bookstoreModel,
+                                      bookView,
+                                      new TestSchedulerProvider(testScheduler));
+    }
+
+    @Test
+    public void bind_doesNotSetsFavoriteBookTitle_whenGetFavoriteBookEmits() {
+        when(bookstoreModel.getFavoriteBook()).thenReturn(Observable.just(Book.create("title")));
+
+        presenter.bind();
+        testScheduler.triggerActions();
+
+        verify(bookView, never()).setBookTitle(anyString());
+    }
+
+    @Test
+    public void bind_setsFavoriteBookTitle_whenGetFavoriteBookEmits_afterDelay() {
+        String title = "The adventures of the Scheduler";
+        when(bookstoreModel.getFavoriteBook()).thenReturn(Observable.just(Book.create(title)));
+
+        presenter.bind();
+        testScheduler.advanceTimeBy(MainPresenter.DELAY_TIME_SEC, TimeUnit.SECONDS);
+
+        verify(bookView).setBookTitle(title);
+    }
+
+}
